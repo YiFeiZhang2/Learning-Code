71a2502416cdf2520b6e23307a42706335c954a1
Richard Zhang <richardzhang97@gmail.com>
2016-10-18 14:58:39 -0400

    Add python script to merge overlap in list of tuple

--- Python/src/tuple_checker.py
+++ Python/src/tuple_checker.py
@@ -1,21 +1,27 @@
-#checks whether tuple output makes sense
+#finds any overlap in tuples, and joins them together
 import re
+import copy
 
-file_name=input("enter file name containing list of tuples\n")
+file_name = input("enter file name containing list of tuples\n")
 read_file = open(file_name, 'r')
 content = re.sub(r'[,\\\s\[\(\)\]\']+', ' ', read_file.read()).strip()
-content_list = content.split(' ')
+content_list = [int(x) for x in content.split(' ')]
 it = iter(content_list)
 tuple_list = list(zip(it, it))
 
-num_weird=0
-weird_list=list()
-print(tuple_list)
+#print(tuple_list)
+ori_length = len(tuple_list)
 
-for i in range(0, len(tuple_list)-1):
-    if tuple_list[i][1] < tuple_list[i+1][0]:
+ind = 0
+while ind < len(tuple_list)-1:
+    if tuple_list[ind][1] < tuple_list[ind+1][0]:
+        ind += 1
         continue
-    if tuple_list[i][1] >= tuple_list[i+1][0]:
-        weird_list.append((tuple_list[i], tuple_list[i+1]))
-
-print(num_weird)
+    elif tuple_list[ind][1] >= tuple_list[ind+1][0]:
+        new_tuple = (tuple_list[ind][0], tuple_list[ind+1][1])
+        tuple_list[ind] = new_tuple
+        prev = copy.deepcopy(tuple_list[:ind+1])
+        next = copy.deepcopy(tuple_list[ind+2:])
+        tuple_list = prev + next
+print(ori_length - len(tuple_list))
+print(tuple_list)
