d560ad927902917bb8073a2579609d8e8f1f21fd
Richard Zhang <richardzhang97@gmail.com>
2017-01-06 19:03:14 -0500

    Add type checking to input

--- F#/n_hello_world.fsx
+++ F#/n_hello_world.fsx
@@ -1,14 +1,20 @@
 //prints out Hello World n times, where
 //n is taken from stdin
-open System;;
-
-let num = Console.ReadLine() |> int;;
+open System
 
 let rec print_hello n: int = 
     if n = 0 
         then 0
     else 
         printfn "Hello World"
-        print_hello (n-1);;
+        print_hello (n-1)
 
-print_hello num;;
+let line = Console.ReadLine()
+
+let mutable num = 0
+
+if ( System.Int32.TryParse( line, &num ) )then 
+    print_hello num
+else 
+    printfn "Wrong input type"
+    0
